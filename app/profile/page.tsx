60a64c4e24693a8be5718fe5d2b553f1aba7e64a
anthony1204 <78404321+anthony1204@users.noreply.github.com>
2024-10-20 19:31:47 +0800

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,6 +8,7 @@ import { Toaster,toast } from "react-hot-toast";
 import ResetMessages from "../_utils/ResetMessages";
 
 // the user data will be retrieved server side
+export const runtime = 'edge';
 export default async function Profile() {
     //start checking tokens
     const cookieStore = cookies();
