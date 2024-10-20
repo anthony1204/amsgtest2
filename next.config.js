a4142e9b44b4908950dc73e814b4e248abc8e60f
anthony1204 <78404321+anthony1204@users.noreply.github.com>
2024-10-20 19:06:47 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
+    output: "export",
     env: {
       CORBADO_API_SECRET: process.env.CORBADO_API_SECRET,
     },
